94b152c78d885a7104fe337a39277153a152fbe8
seanrtprime <149831736+seanrtprime@users.noreply.github.com>
2023-12-06 15:35:44 -0800

    Delete ChocoPrereq.ps1

--- ChocoPrereq.ps1
+++
@@ -1,9 +0,0 @@
-# Install Chocolatey
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-
-# Add Chocolatey to PATH
-$chocoPath = Join-Path $env:ProgramData 'chocolatey\bin'
-$existingPath = [System.Environment]::GetEnvironmentVariable('PATH', [System.EnvironmentVariableTarget]::Machine)
-if ($existingPath -notcontains $chocoPath) {
-    [System.Environment]::SetEnvironmentVariable('PATH', "$existingPath;$chocoPath", [System.EnvironmentVariableTarget]::Machine)
-}
